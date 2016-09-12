eb452cd61823788ef29656b56f23d4cfcfc1a78d
Alwin Arrasyid <alwin.ridd@gmail.com>
2016-09-12 22:29:24 +0700

    Added manhattan distance function

--- algorithm/knn/KNNClassifier.js
+++ algorithm/knn/KNNClassifier.js
@@ -17,7 +17,25 @@ const distance = {
         }
 
         return Math.sqrt(result);
-    }
+    },
+
+    manhattan(onePoint, anotherPoint) {
+        if (!Array.isArray(onePoint) || !Array.isArray(anotherPoint)) {
+            throw new Error('Both points must be array!');
+        }
+
+        if (onePoint.length !== anotherPoint.length) {
+            throw new Error('The two points must share the same dimension!');
+        }
+
+        let result = 0;
+
+        for (let i = 0; i < onePoint.length; i++) {
+            result += Math.abs(onePoint[i] - anotherPoint[i]);
+        }
+
+        return result;
+    },
 }
 
 function selectLabel(results, n) {
